8f0babef841883b8b9f7000a4454800c9da7f190
kent <abitonakent923@gmail.com>
2026-05-25 21:45:14 +0800

    Support Cloudinary configuration from URL or individual credentials

--- photo_album_project/settings.py
+++ photo_album_project/settings.py
@@ -104,10 +104,27 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': env('CLOUDINARY_API_SECRET', ''),
 }
 
-if CLOUDINARY_URL:
+CLOUDINARY_CONFIGURED = bool(
+    CLOUDINARY_URL
+    or (
+        CLOUDINARY_STORAGE['CLOUD_NAME']
+        and CLOUDINARY_STORAGE['API_KEY']
+        and CLOUDINARY_STORAGE['API_SECRET']
+    )
+)
+
+if CLOUDINARY_CONFIGURED:
     import cloudinary
 
-    cloudinary.config(cloudinary_url=CLOUDINARY_URL)
+    if CLOUDINARY_URL:
+        cloudinary.config(cloudinary_url=CLOUDINARY_URL)
+    else:
+        cloudinary.config(
+            cloud_name=CLOUDINARY_STORAGE['CLOUD_NAME'],
+            api_key=CLOUDINARY_STORAGE['API_KEY'],
+            api_secret=CLOUDINARY_STORAGE['API_SECRET'],
+        )
+
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
